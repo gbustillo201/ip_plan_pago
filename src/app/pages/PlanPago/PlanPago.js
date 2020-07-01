58d078b406799c81d80d3e4ce775d6e30c48b8bf
dantedubon <dante.dubon@gmail.com>
2020-06-30 20:04:27 -0600

    Moving and centering button

--- src/app/pages/PlanPago/PlanPago.js
+++ src/app/pages/PlanPago/PlanPago.js
@@ -9,20 +9,24 @@ import QRCode from "qrcode.react";
 import moment from 'moment';
 import { useReactToPrint } from 'react-to-print';
 
-const Header = ({ planId }) => {
+const Header = ({ planId, hasError }) => {
 
     const url = `${process.env.REACT_APP_PLAN}/${planId}`;
+    const justify = hasError? 'justify-content-center': 'justify-content-between'
     return (
-        <div className="d-flex justify-content-between p-3">
+        <div className={`d-flex ${justify} p-3`}>
             <img
                 className="card-media  d-block"
                 src={process.env.PUBLIC_URL + "/logo IP.png"}
                 alt=""
             />
-            <div className="d-flex flex-column align-items-center">
-                <QRCode className="qr-box" value={url} />
-                <p className="plan-id">Nº {`${planId}`} </p>
-            </div>
+            {
+
+                !hasError && <div className="d-flex flex-column align-items-center">
+                    <QRCode className="qr-box" value={url} />
+                    <p className="plan-id">Nº {`${planId}`} </p>
+                </div>
+            }
 
         </div>
     )
@@ -70,53 +74,53 @@ const DatosGenerales = ({ placa, rtn, propietario, solicitanteNombre, solicitant
     )
 }
 
-const DatosRecepcion = ({dateLegend}) => {
+const DatosRecepcion = ({ dateLegend }) => {
     return (
         <>
-        <h5 className="text-center plan-datos-title mt-5">Datos de Recepcion</h5>
+            <h5 className="text-center plan-datos-title mt-5">Datos de Recepcion</h5>
 
 
-        <div className="table-responsive">
-            <table className="table">
-                <thead>
-                    <tr className="plan-datos-header">
-                        <th>
-                            Fecha
+            <div className="table-responsive">
+                <table className="table">
+                    <thead>
+                        <tr className="plan-datos-header">
+                            <th>
+                                Fecha
                         </th>
-                        <th>
-                            Id Funcionario
+                            <th>
+                                Id Funcionario
                         </th>
-                        <th>
-                            Nombre Funcionario
+                            <th>
+                                Nombre Funcionario
                         </th>
-                       
-                    </tr>
 
-                </thead>
-                <tbody>
-                    <tr className="plan-datos-items">
-                        <td className="pt-0">{dateLegend}</td>
-                        <td className="pt-0">{"888888"}</td>
-                        <td className="pt-0">{"Web"}</td>
-                        
-                    </tr>
-                </tbody>
-            </table>
+                        </tr>
 
-        </div>
-    </>
+                    </thead>
+                    <tbody>
+                        <tr className="plan-datos-items">
+                            <td className="pt-0">{dateLegend}</td>
+                            <td className="pt-0">{"888888"}</td>
+                            <td className="pt-0">{"Web"}</td>
+
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
+        </>
     )
 }
 
-const Observaciones = () =>{
+const Observaciones = () => {
     return <div className="observaciones mt-5">
         <p >Observación:</p>
         <p className="text-justify">La falta de pago de dos (2) cuotas en los plazos previstos en el presente Convenio de Pago, será causal suficiente para que el Instituto de la Propiedad prosiga su cobro por la vía legal, sin perjuicio de otras acciones administrativas como la publicación del adeudo en la página de la Institución.</p>
         <p className="text-justify">"Además me comprometo a pagar la mora que se encuentra registrada en el sistema y que no se incluyo en el presente plan"</p>
-        <p className="text-center  firmas">Nombre y Firma del Usuario o <br/>Representante Legal</p>
+        <p className="text-center  firmas">Nombre y Firma del Usuario o <br />Representante Legal</p>
         <div className="d-flex justify-content-between flex-wrap">
-            <p className="text-lg-left text-center firmas">Nombre y Firma del Oficial <br/> de Planes de Pago</p>
-            <p className="firmas"> Nombre y Firma Gerente de Registro Vehicular o <br/> Gerente de Centro de Atención</p>
+            <p className="text-lg-left text-center firmas">Nombre y Firma del Oficial <br /> de Planes de Pago</p>
+            <p className="firmas"> Nombre y Firma Gerente de Registro Vehicular o <br /> Gerente de Centro de Atención</p>
 
         </div>
     </div>
@@ -141,7 +145,7 @@ const PlanPagos = () => {
 
 
     const dateLegend = (planPagoDate) => {
-        
+
         return moment(planPagoDate).format('DD/MM/YYYY, h:mm:ss a')
     }
 
@@ -152,15 +156,15 @@ const PlanPagos = () => {
     }
 
     const componentRef = useRef();
-    
+
     const handlePrint = useReactToPrint({
         content: () => componentRef.current,
-       
-      });
+
+    });
 
     return (
         <div className="card w-100 PrintPlan" ref={componentRef}>
-            <Header planId={planId} />
+            <Header planId={planId} hasError={error} />
 
 
             {
@@ -168,20 +172,24 @@ const PlanPagos = () => {
                     <div id="subscription">
                         <h1 className="card-title plan-pago-title text-center ">Convenio de Plan de Pago</h1>
 
-                        <button id="btn-print" className="btn btn-primary" onClick={handlePrint}>Imprimir Plan de Pago</button>
+                      
                         <div className="plan-pago mt-5">
                             <DatosGenerales placa={placa} rtn={rtn} propietario={propietario} solicitanteNombre={solicitanteNombre} solicitanteIdentificacion={solicitanteIdentificacion} />
 
                             <div className="plan-pago-estado-cuenta">
                                 <div className="plan-pagos-table">
-                                <EstadoCuenta estadoCuenta={estadoCuenta} />                              
-                                <ProyeccionMultas proyeccionMultas={proyeccionMultas} />
-                                <DistribucionCuotas distribucion={mapDistribucionCuotas(distribucionCuotas)} />
+                                    <EstadoCuenta estadoCuenta={estadoCuenta} />
+                                    <ProyeccionMultas proyeccionMultas={proyeccionMultas} />
+                                    <DistribucionCuotas distribucion={mapDistribucionCuotas(distribucionCuotas)} />
+                                </div>
+
+                                <DatosRecepcion dateLegend={dateLegend(fechaEmision)} />
+                                <Observaciones />
+                                <div className="d-flex justify-content-center"> 
+                                <button id="btn-print" className="btn btn-primary" onClick={handlePrint}>Imprimir Plan de Pago</button>
                                 </div>
                                 
-                                <DatosRecepcion dateLegend={dateLegend(fechaEmision)}/>
-                                <Observaciones/>
-                                
+
                             </div>
 
                         </div>
